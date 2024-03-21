eea54cfb59e22b0c9d6ee811ff0815fbe0e4193b
ATSISTEMAS\jldorado <jldorado@atsistemas.com>
2024-03-21 16:51:14 +0100

    final

--- 03-bundling/entregable/src/helloComponent.tsx
+++ 03-bundling/entregable/src/helloComponent.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./hello.module.scss";
+import logoImg from "./content/logo.png";
 
 export const HelloComponent = () => {
     return (
         <div>
             <p>Hello from React Component</p>
             <p className={`${styles.textColor}`}>Hello with sass</p>
+            <img src={logoImg} />
         </div>
     );
 };
