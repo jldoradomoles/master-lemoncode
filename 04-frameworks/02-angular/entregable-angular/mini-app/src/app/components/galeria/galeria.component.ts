fdcd93b269ddfbfd83f3c64055cc801dd46ad88c
jose luis dorado <jldoradomoles@gmail.com>
2024-05-22 19:00:34 +0200

    imagenes

--- 04-frameworks/02-angular/entregable-angular/mini-app/src/app/components/galeria/galeria.component.ts
+++ 04-frameworks/02-angular/entregable-angular/mini-app/src/app/components/galeria/galeria.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import Imagenes, { Imagen } from './imagenes';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-galeria',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, MatButtonModule, MatIconModule],
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.scss',
 })
-export class GaleriaComponent {}
+export class GaleriaComponent implements OnInit {
+  imagenes: Imagen[] = [];
+
+  ngOnInit(): void {
+    this.imagenes = Imagenes;
+  }
+}
--- 04-frameworks/02-angular/entregable-angular/mini-app/src/app/components/galeria/imagenes.ts
+++ 04-frameworks/02-angular/entregable-angular/mini-app/src/app/components/galeria/imagenes.ts
@@ -1,21 +1,50 @@
 export interface Imagen {
   id: number;
   src: string;
-  descripcion: string;
+  title: string;
 }
 
-const imagenes: Imagen[] = [
+const Imagenes: Imagen[] = [
   {
     id: 1,
-    src: 'https://example.com/image1.jpg',
-    descripcion: 'Imagen 1',
+    src: '/assets/imagenes/deku.jpg',
+    title: 'Imagen 1',
   },
   {
     id: 2,
-    src: 'https://example.com/image2.jpg',
-    descripcion: 'Imagen 2',
+    src: '/assets/imagenes/gojo.jpg',
+    title: 'Imagen 2',
   },
-  // Add more objects here...
+  {
+    id: 3,
+    src: '/assets/imagenes/ichigo.jpg',
+    title: 'Imagen 3',
+  },
+  {
+    id: 4,
+    src: '/assets/imagenes/naruto.jpg',
+    title: 'Imagen 4',
+  },
+  {
+    id: 5,
+    src: '/assets/imagenes/spike.jpg',
+    title: 'Imagen 5',
+  },
+  {
+    id: 6,
+    src: '/assets/imagenes/tanjiro.jpg',
+    title: 'Imagen 6',
+  },
+  {
+    id: 7,
+    src: '/assets/imagenes/vash.jpg',
+    title: 'Imagen 7',
+  },
+  {
+    id: 8,
+    src: '/assets/imagenes/gabimaru.jpg',
+    title: 'Imagen 8',
+  },
 ];
 
-export default imagenes;
+export default Imagenes;
