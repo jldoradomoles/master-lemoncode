d75d8031a022b048ea625efd3a031c51204687f5
jose luis dorado <jldoradomoles@gmail.com>
2024-05-28 19:42:01 +0200

    prev next imagenes

--- 04-frameworks/02-angular/entregable-angular/mini-app/src/app/components/galeria/galeria.component.ts
+++ 04-frameworks/02-angular/entregable-angular/mini-app/src/app/components/galeria/galeria.component.ts
@@ -14,7 +14,49 @@ import { MatIconModule } from '@angular/material/icon';
 export class GaleriaComponent implements OnInit {
   imagenes: Imagen[] = [];
 
+  imagenInit: number = 0;
+  imagenEnd: number = 3;
+  isLastImage: boolean = false;
+  isFirstImage: boolean = false;
+
   ngOnInit(): void {
-    this.imagenes = Imagenes;
+    this.getThreeFirstImages();
+  }
+
+  getThreeFirstImages(): void {
+    this.imagenes = Imagenes.slice(0, 3);
+  }
+
+  nextImages(): void {
+    this.imagenInit += 1;
+    this.imagenEnd += 1;
+    console.log(this.imagenEnd, this.imagenInit);
+    if (this.imagenEnd <= 8) {
+      this.imagenes = Imagenes.slice(this.imagenInit, this.imagenEnd);
+      this.isFirstImage = false;
+    }
+    if (this.imagenEnd == 8) {
+      this.isLastImage = true;
+    }
+  }
+
+  previousImages(): void {
+    if (this.imagenInit >= 1) {
+      this.imagenInit -= 1;
+    }
+    this.imagenEnd -= 1;
+
+    console.log(this.imagenEnd, this.imagenInit);
+
+    if (this.imagenInit >= 0) {
+      this.imagenes = Imagenes.slice(this.imagenInit, this.imagenEnd);
+    }
+    if (this.imagenInit === 0) {
+      this.isFirstImage = true;
+    }
+
+    // this.imagenInit >= 0
+    //   ? (this.imagenes = Imagenes.slice(this.imagenInit, this.imagenEnd))
+    //   : (this.isFirstImage = true);
   }
 }
